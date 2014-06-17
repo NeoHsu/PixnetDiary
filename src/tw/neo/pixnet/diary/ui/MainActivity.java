efafdb9abb3ad93658ee7167afcde8285225b577
Neo Hsu <NeoHsuDroid@gmail.com>
2014-06-17 18:41:11 +0800

    Added ListView Item UI

--- src/tw/neo/pixnet/diary/ui/MainActivity.java
+++ src/tw/neo/pixnet/diary/ui/MainActivity.java
@@ -5,24 +5,66 @@
 
 package tw.neo.pixnet.diary.ui;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import tw.neo.pixnet.diary.R;
+import tw.neo.pixnet.diary.util.BaseRowItem;
 
 public class MainActivity extends BaseActivity {
     private static final String TAG = "MainActivity";
+    private ListView listView;
+    private List<BaseRowItem> rowItems;
+    private Context mCtx;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_main);
-
+        mCtx = MainActivity.this;
         if (savedInstanceState == null) {
-
+            findViews();
+            setListView();
         }
+    }
+
+    private void findViews() {
+        Log.d(TAG, "findViews");
+        listView = (ListView) findViewById(R.id.list);
+    }
+
+    private void setListView() {
+        Log.d(TAG, "setListView");
+        rowItems = new ArrayList<BaseRowItem>();
+        for (int i = 0; i < 10; i++) {
+            BaseRowItem item = new BaseRowItem("test", "test");
+            rowItems.add(item);
+        }
+        BaseCustomAdapter adapter = new BaseCustomAdapter(mCtx, rowItems);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+    }
+
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+
     }
 
     @Override
